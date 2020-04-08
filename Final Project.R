d8379dd078e62de2ee82a0286a80839d3de23765
dsk5jy <63305103+dsk5jy@users.noreply.github.com>
2020-04-08 13:36:43 -0400

    Add files via upload

--- Final Project.R
+++ Final Project.R
@@ -14,8 +14,7 @@ library(dplyr)
 library(cluster)
 library(sf)
 library(ggfortify)
-install.packages("zipcode")
-library(zipcode)
+
 #-----------------Get the US county map data and then extract the Virginia counties-----------
 counties<-map_data("county")
 va_county<-counties%>%filter(region=="virginia")
@@ -23,23 +22,28 @@ va_county<-counties%>%filter(region=="virginia")
 vamap<- ggplot(data=va_county, mapping=aes(x=long, y=lat, group=group))+
   coord_quickmap()+geom_polygon(fill="white", color="black", size=.2)
 vamap
-#------------- Import arrest data----------
-arrests <- read_excel("Downloads/ArrestData_Cville_2019-pres_CLEAN.xlsx")
-arrests<-separate(arrests, TIME, into=c("bullshitdate", "time"), sep=" " )
-arrests$bullshitdate<-NULL
-arrests$HOUR<-NULL
-arrests$MINUTE<-NULL
-arrests$SECOND<-NULL
-arrests$FirstName<-NULL
-arrests$LastName<-NULL
-arrests$MiddleName<-NULL
-arrests$NameSuffix<-NULL
-arrests<-separate(arrests, time, into=c("hour", "minute", "second"), sep=":")
-
+#------------- Import arrest data and add city and state data----------
 arrests <- read.csv("~/Desktop/arrests-blue.csv")
 arrests$address=arrests$ADDRESS
 arrests$city= "Charlottesville"
 arrests$state= "VA"
-arrests$zipcode= "22903"
-arrests<-arrests%>%unite("FullAddress", city:zipcode, sep=", ")
+arrests<-arrests %>% unite("FullAddress", city:state, sep=", ")
 arrests<-arrests %>% unite("CompleteAddress", address:FullAddress, sep=", ")
+#------Assign crime scores----------------
+arrests$crimescore=" "
+
+#-------------- get geo coordinates from address----------------
+geocoded<-data.frame(arrests)
+register_google(key="AIzaSyBtSvnbnZzZdQ3K93rjCkW56mY6iXKC0XA")
+has_google_key()
+for(i in 1:nrow(arrests)){
+  result<-geocode(arrests$CompleteAddress[i], output="latlona", source="google")
+  arrests$longitude[i]<-as.numeric(result[1])
+  arrests$latitude[i]<-as.numeric(result[2])
+}
+#--------------Omit the NA coordinate values-------------
+arrests<-na.omit(arrests)
+#---- plot the crime data on the map
+cville_map <- get_map(getbb("Charlottesville"), maptype="toner-background")
+crimemap<-ggmap(cville_map)+geom_point(data=arrests, aes(x=arrests$longitude, y=arrests$latitude),color="blue")
+crimemap
