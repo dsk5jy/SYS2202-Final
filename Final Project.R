6d13b4a50d1b73e01b64c460392daa9cefb9d8cc
dsk5jy <63305103+dsk5jy@users.noreply.github.com>
2020-04-09 11:49:21 -0400

    Add files via upload

--- Final Project.R
+++ Final Project.R
@@ -22,28 +22,23 @@ va_county<-counties%>%filter(region=="virginia")
 vamap<- ggplot(data=va_county, mapping=aes(x=long, y=lat, group=group))+
   coord_quickmap()+geom_polygon(fill="white", color="black", size=.2)
 vamap
-#------------- Import arrest data and add city and state data----------
-arrests <- read.csv("~/Desktop/arrests-blue.csv")
-arrests$address=arrests$ADDRESS
-arrests$city= "Charlottesville"
-arrests$state= "VA"
-arrests<-arrests %>% unite("FullAddress", city:state, sep=", ")
-arrests<-arrests %>% unite("CompleteAddress", address:FullAddress, sep=", ")
-#------Assign crime scores----------------
-arrests$crimescore=" "
-
-#-------------- get geo coordinates from address----------------
-geocoded<-data.frame(arrests)
+#------------- Import arrest data and fuck w the columns----------
+arrests2 <- read_excel("Downloads/ArrestData_Cville_2019-pres_CLEAN (1).xlsx")
+arrests2$fulladdress<-arrests2$FULL_ADDRESS
+arrests2$FULL_ADDRESS<-NULL
+arrests2$NameSuffix<-NULL
+#--- get geo coordinates from address---------
+geocoded2<-data.frame(arrests2)
 register_google(key="AIzaSyBtSvnbnZzZdQ3K93rjCkW56mY6iXKC0XA")
 has_google_key()
-for(i in 1:nrow(arrests)){
-  result<-geocode(arrests$CompleteAddress[i], output="latlona", source="google")
-  arrests$longitude[i]<-as.numeric(result[1])
-  arrests$latitude[i]<-as.numeric(result[2])
+for(i in 1:nrow(arrests2)){
+  result<-geocode(arrests2$fulladdress[i], output="latlona", source="google")
+  arrests2$longitude[i]<-as.numeric(result[1])
+  arrests2$latitude[i]<-as.numeric(result[2])
 }
-#--------------Omit the NA coordinate values-------------
-arrests<-na.omit(arrests)
+arrests2<-na.omit(arrests2)
 #---- plot the crime data on the map
 cville_map <- get_map(getbb("Charlottesville"), maptype="toner-background")
-crimemap<-ggmap(cville_map)+geom_point(data=arrests, aes(x=arrests$longitude, y=arrests$latitude),color="blue")
+crimemap<-ggmap(cville_map)+geom_point(data=arrests2, aes(x=arrests2$longitude, y=arrests2$latitude),color="blue")
 crimemap
+write.csv(arrests2, "Desktop/ArrestData_Cville_2019_WithCoordinates")
