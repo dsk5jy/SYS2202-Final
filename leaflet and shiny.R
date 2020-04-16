5af73a52f63665857db1c4c9f7523e3d7a394a32
jts2gt <63305648+jts2gt@users.noreply.github.com>
2020-04-16 13:06:45 -0400

    Update leaflet and shiny.R

--- leaflet and shiny.R
+++ leaflet and shiny.R
@@ -30,6 +30,62 @@ server <- function(input, output, session) {
 
 shinyApp(ui, server)
 
+# ---------------------------------------------------------------------
+# working buttons with leaflet map - Jackson 
+  
+ui <- fluidPage(
+  titlePanel("Charlottesville Crime Map"),
+  verticalLayout(sidebarPanel("OPTIONS", 
+                              fluidRow(
+     column(3, sliderInput("slider", "Time of Day", min = 0, max = 23, value = cData$hour)),
+      column(3, checkboxGroupInput("gender", "Gender", c("Male", "Female"), select = "Male")),
+      column(3, sliderInput("repeet", "Repeat Offenses", min= 1, max = 20, value = cData$NumOffenses)),
+      column(3, checkboxGroupInput("violence", "Violence Level", c("1", "2", "3", "4", "5"))),
+     column(3, checkboxGroupInput("race", "RACE", c("White", "Black", "Asian or Pacific Islander", "Not White"), selected= "White")),
+     column(3, dateRangeInput("date", "DATE", start= "2019-01-01", end= "2020-04-16", format= "mm/dd/yy"))),
+     width = 12),
+  
+  mainPanel("Crime Map", leafletOutput("mymap"))
+ )
+)
+  
+server <- function(input, output, session){
+  
+  
+  output$mymap <- renderLeaflet({
+    leaflet() %>% 
+      addTiles() %>%
+      addMarkers(lng = cData$longitude, lat= cData$latitude)
+  })
+
+  observe({
+    
+    hour <- input$hour
+    offenses <- input$repeet
+    gender <- input$gender
+    date <- input$date
+    violence <- input$violence
+    race <- input$race
+    
+    newPoints <- cData %>%
+      filter(cData$hour %in% hour & cData$NumOffenses %in% offenses & cData$Sex %in% gender & cData$VIOLENCE %in% violence & cData$Race %in% race)
+    
+    leafletProxy("mymap") %>% clearMarkers() %>%
+      addMarkers(lng = newPoints$longitude, lat= newPoints$latitude)
+  })
+
+}
+
+shinyApp(ui, server)
+
+
+
+
+
+
+
+
+# --------------------------------------------------------------------
 
 
 install.packages("DT")
