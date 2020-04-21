9ed304a61acd8c90f6321b0a48a4eca6f3c35e37
jts2gt <63305648+jts2gt@users.noreply.github.com>
2020-04-21 11:19:07 -0400

    Update leaflet and shiny.R

--- leaflet and shiny.R
+++ leaflet and shiny.R
@@ -105,3 +105,58 @@ for (i in 1:nrow(ArrestData_Cville_2019_pres_CLEAN)){
   ArrestData_Cville_2019_pres_CLEAN$lat[i] <- as.numeric(result[2])
   ArrestData_Cville_2019_pres_CLEAN$geoAddress[i] <- as.character(result[3])
 }
+
+# ---------------------------------------------
+
+# with popups 
+
+  
+ui <- fluidPage(
+  titlePanel("Charlottesville Crime Map"),
+  verticalLayout(sidebarPanel("OPTIONS", 
+                              fluidRow(
+     column(3, sliderInput("slider", "Time of Day", min = 0, max = 23, value = cData$hour)),
+      column(3, checkboxGroupInput("gender", "Gender", c("Male", "Female"), select = "Male")),
+      column(3, sliderInput("repeet", "Repeat Offenses", min= 1, max = 20, value = cData$NumOffenses)),
+      column(3, checkboxGroupInput("violence", "Violence Level", c("1", "2", "3", "4", "5"))),
+     column(3, checkboxGroupInput("race", "RACE", c("White", "Black", "Asian or Pacific Islander", "Not White"), selected= "White")),
+     column(3, dateRangeInput("date", "DATE", start= "2019-01-01", end= "2020-04-16", format= "mm/dd/yy"))),
+     width = 12),
+  
+  mainPanel("Crime Map", leafletOutput("mymap"))
+ )
+)
+  
+server <- function(input, output, session){
+  
+  
+  output$mymap <- renderLeaflet({
+    leaflet() %>% 
+      addTiles() %>%
+      addMarkers(lng = cData$longitude, lat= cData$latitude)
+  })
+
+  observe({
+    
+    hour <- input$hour
+    offenses <- input$repeet
+    gender <- input$gender
+    date <- input$date
+    violence <- input$violence
+    race <- input$race
+    
+    newPoints <- cData %>%
+      filter(cData$hour %in% hour & cData$NumOffenses %in% offenses & cData$Sex %in% gender & cData$VIOLENCE %in% violence & cData$Race %in% race)
+    
+    leafletProxy("mymap") %>% clearMarkers() %>%
+      addMarkers(lng = newPoints$longitude, lat= newPoints$latitude, popup = paste("ArrestID", cData$ArrestID, "<br>",
+                                                                                   "ArrestDate:", cData$ArrestDate, "<br>",
+                                                                                   "Gender:", cData$Sex, "<br>",
+                                                                                   "Race:", cData$Race))
+  })
+
+}
+
+shinyApp(ui, server)
+
+
