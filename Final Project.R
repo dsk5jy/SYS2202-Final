6cd9a11768457cac0c282cd2e62b3bfe823a8963
dsk5jy <63305103+dsk5jy@users.noreply.github.com>
2020-04-22 13:04:24 -0400

    Add files via upload

--- Final Project.R
+++ Final Project.R
@@ -14,14 +14,19 @@ library(dplyr)
 library(cluster)
 library(sf)
 library(ggfortify)
-
+library(shiny)
+library(leaflet)
+library(leaflet.extras)
 #-----------------Get the US county map data and then extract the Virginia counties-----------
-counties<-map_data("county")
+counties<-map_data("state")
 va_county<-counties%>%filter(region=="virginia")
 #----------Plot the Virginia counties Map-------------------
 vamap<- ggplot(data=va_county, mapping=aes(x=long, y=lat, group=group))+
   coord_quickmap()+geom_polygon(fill="white", color="black", size=.2)
+uscountymap<-ggplot(data=counties, mapping=aes(x=long, y=lat, group=group))+
+  coord_quickmap()+geom_polygon(fill="white", color="black", size=.2)
 vamap
+uscountymap
 #------------- Import arrest data and fuck w the columns----------
 arrests2 <- read_excel("Downloads/ArrestData_Cville_2019-pres_CLEAN (1).xlsx")
 arrests2$fulladdress<-arrests2$FULL_ADDRESS
@@ -42,8 +47,12 @@ for(i in 1:nrow(arrests2)){
 }
 arrests2<-na.omit(arrests2)
 #---- plot the crime data on the map
-cville_map <- get_map(getbb("Charlottesville"), maptype="toner-background")
-crimemap<-ggmap(cville_map)+geom_point(data=arrests2, aes(x=arrests2$longitude, y=arrests2$latitude),color="blue")
+usmap<-get_map(getbb("United States"), maptype="toner-background")
+cville_map <- get_map(location="united states ", maptype="toner-background")
+mapjoint<- ggmap(cville_map)+geom_point(data=arrests2, aes(x=arrests2$longitude, y=arrests2$latitude),color="blue")
+mapjoint
+crimemap<-uscountymap+
+  geom_point(data=arrests2, aes(x=arrests2$longitude, y=arrests2$latitude),color="blue", group=group)
 crimemap
 #--- write to excel to fuck w some stuff
 write.csv(arrests2, "Desktop/ArrestData_Cville_2019_WithCoordinates.csv")
@@ -51,140 +60,74 @@ write.csv(arrests2, "Desktop/ArrestData_Cville_2019_WithCoordinates.csv")
 arrests2<- read.csv("~/Desktop/ArrestData_Cville_2019_WithCoordinates.csv")
 
 ### Everything above written by Darren
-
+#----------- Fuck w Shiny and Leaflet --------------------------
 arrests2$violencelev <- ifelse(arrests2$VIOLENCE <=2, "mild",
                                ifelse(arrests2$VIOLENCE >2 | arrests2$VIOLENCE <5,"medium",
                                       ifelse(arrests2$VIOLENCE >= 5, "Bad")))
 
+#---- clear outlying values- use the map to look where the values are-----
 
+arrests2<-arrests2[-c(1633, 1634, 1635, 1636, 3295, 3296, 3297, 1445, 1922, 1372, 586, 607, 1213, 1214),]
+arrests3<-arrests2[-607]
+bbox_USA = c(left = -124.7844079,
+             bottom = 24.7433195,
+             right = -66.9513812,
+             top = 49.3457868)
 
+USA = get_map(bbox_USA, source='stamen')
+jaunt<-ggmap(USA)+geom_point(data=arrests3, aes(x=longitude, y=latitude),color="blue")
+jaunt
+for (i in length(arrests2)){
+  arrests2$longitude[arrests2$longitude %in% "-79.94764"] <-NA
+}
+arrests2<-na.omit(arrests2)
+min(arrests2$longitude)
+
+#--------- shiny app creation------------
 ui <- fluidPage(
-  mainPanel(
-    leafletOutput(outputId = "mymap"),
-    absolutePanel(top = 60, left = 20,
-                  checkboxInput("markers","VIOLENCE",FALSE),
-                  checkboxInput("heat","violencelev",FALSE)
-                  
-                )
+  titlePanel("Charlottesville Crime Map"),
+  verticalLayout(sidebarPanel("OPTIONS", 
+                              fluidRow(
+                                column(3, sliderInput("slider", "Time of Day", min = 0, max = 23, value = arrests2$hour)),
+                                column(3, checkboxGroupInput("gender", "Gender", c("Male", "Female"), select = "Male")),
+                                column(3, sliderInput("repeet", "Repeat Offenses", min= 1, max = 20, value = arrests2$NumOffenses)),
+                                column(3, checkboxGroupInput("violence", "Violence Level", c("1", "2", "3", "4", "5"))),
+                                column(3, checkboxGroupInput("race", "RACE", c("White", "Black", "Asian or Pacific Islander", "Not White"), selected= "White")),
+                                column(3, dateRangeInput("date", "DATE", start= "2019-01-01", end= "2020-04-16", format= "mm/dd/yy"))),
+                              width = 12),
+                 
+                 mainPanel("Crime Map", leafletOutput("mymap"))
   )
 )
 
-server <- function(input,output,session){
-  pal<- colorNumeric(
-    palette = c('green','yellow','red'),
-                domain = arrests2$VIOLENCE)
-  pal2 <- colorFactor(
-    palette = c('green','yellow','red'),
-    domain = arrests2$violencelev
-  )
+server <- function(input, output, session){
+  
   
   output$mymap <- renderLeaflet({
-    leaflet(arrests2) %>%
-      setView(lng = -78.49,lat = 38.03, zoom = 2) %>%
+    leaflet() %>% 
       addTiles() %>%
-      addCircles(data = arrests2, lat = ~ latitude, lng = ~ longitude,
-                 weight = 1, radius = ~sqrt(VIOLENCE)*40, popup = ~as.character(VIOLENCE),
-                 label = ~as.character(paste0("Violence: ",sep = " ", VIOLENCE)), color = ~ pal(VIOLENCE),fillOpacity = 0.5)
+      addMarkers(lng = arrests2$longitude, lat= arrests2$latitude)
   })
   
   observe({
-    proxy<- leafletProxy("mymap",data = arrests2)
-    proxy %>% clearMarkers()
-    if(input$markers){
-      proxy %>% addCircleMarkers(stroke = FALSE, color = ~ pal2(violencelev), fillOpacity = 0.2,
-                    label = ~as.character(paste0("Violence: ", sep = " ", VIOLENCE)))%>%
-                      addLegend("bottomright", pal = pal2, values = arrests2$violencelev,
-                                title = "Violence",
-                                opacity = 1)}
-    else{
-      proxy %>% clearMarkers() %>% clearControls()
-    }
-  })
-  
-  observe({
-    proxy<- leafletProxy("mymap", data = arrests2)
-    proxy %>% clearMarkers()
-    if(input$heat){
-      proxy %>% addHeatmap(lng = ~longitude, lat = ~latitude, 
-        intensity = ~VIOLENCE, blur = 10, max = 0.05, radius = 15)
-    }
-    else{
-      proxy %>% clearHeatmap()
-    }
+    
+    hour <- input$hour
+    offenses <- input$repeet
+    gender <- input$gender
+    date <- input$date
+    violence <- input$violence
+    race <- input$race
+    
+    newPoints <- arrests2 %>%
+      filter(arrests2$ArrestDate %in% date & arrests2$hour %in% hour & arrests2$NumOffenses %in% offenses & arrests2$Sex %in% gender & arrests2$VIOLENCE %in% violence & arrests2$Race %in% race)
+    
+    leafletProxy("mymap") %>% clearMarkers() %>%
+      addMarkers(lng = newPoints$longitude, lat= newPoints$latitude, popup = paste("ArrestID", arrests2$ArrestID, "<br>",
+                                                                                   "ArrestDate:", arrests2$ArrestDate, "<br>",
+                                                                                   "Gender:", arrests2$Sex, "<br>",
+                                                                                   "Race:", arrests2$Race))
   })
   
 }
 
-shinyApp(ui,server)
-
-#Code above by hunter diminick
-
-
-#------- HEAT MAP TRIAL CODE ---------# by Emma don't even worry abt it yet it's still in progress
-# HEATMAPPING
-heatmap_time <- NULL
-heatmap_time = data.frame(arrests2$TIME_OF_DAY, arrests2$VIOLENCE, arrests2$NumOffenses)
-
-
-# Nomalizing the data for the heat map representation
-normalize_violence <- function(x) {
-  return ((x - (1)) / ((5) - (0)))
-}
-normalize_numOf <- function(x) {
-  return ((x - (1)) / ((max(arrests2$NumOffenses)) - (1)))
-}
-
-#getting the numerical data from the split dataset
-library(readxl)
-UnstackedArrestData <- read_excel("C:/Users/student/OneDrive/Documents/sys 2202/UnstackedArrestData.xlsx")
-df <- UnstackedArrestData
-heatmap_data <- data.frame(UnstackedArrestData$Violence_ASSAULT,UnstackedArrestData$NumOffenses_ASSAULT,
-                           UnstackedArrestData$Violence_ALCOHOL,UnstackedArrestData$NumOffenses_ALCOHOL,
-                           UnstackedArrestData$Violence_DAMAGE, UnstackedArrestData$NumOffenses_DAMAGE,
-                           UnstackedArrestData$Violence_VIOLATION,UnstackedArrestData$NumOffenses_VIOLATION)
-assault_violence_norm <- normalize_violence(UnstackedArrestData$Violence_ASSAULT)
-assault_offenses_norm <- normalize_numOf(UnstackedArrestData$NumOffenses_ASSAULT)
-alcohol_violence_norm <- normalize_violence(UnstackedArrestData$Violence_ALCOHOL)
-alcohol_offenses_norm <- normalize_numOf(UnstackedArrestData$NumOffenses_ALCOHOL)
-damage_violence_norm <- normalize_violence(UnstackedArrestData$Violence_DAMAGE)
-damage_offenses_norm <- normalize_numOf(UnstackedArrestData$NumOffenses_DAMAGE)
-violation_violence_norm <- normalize_violence(UnstackedArrestData$Violence_VIOLATION)
-violation_offenses_norm <- normalize_numOf(UnstackedArrestData$NumOffenses_VIOLATION)
-heatmap_data_norm <- data.frame(assault_violence_norm, alcohol_violence_norm,damage_violence_norm,violation_violence_norm,
-                                assault_offenses_norm,alcohol_offenses_norm,damage_offenses_norm,violation_offenses_norm)
-heatmap_data_norm <- na.omit(heatmap_data_norm)
-
-
-# Making 4 heatmaps by time intervals and violence level with respect to the number of offenses
-#install.packages("pheatmap")
-library("pheatmap")
-pheatmap(as.matrix(heatmap_data_norm), colors = "RdBu")
-
-  #install.packages("tidyr")
-library("tidyr")
-library("ggplot2")
-arrests2$sqrt.num <- sqrt(arrests2$NumOffenses)
-
-# By Time of Day
-time.heatmap <- ggplot(data = arrests2, mapping = aes(x = arrests2$TIME_OF_DAY, y = arrests2$VIOLENCE, 
-                                                          fill= arrests2$sqrt.num)) + geom_tile() +
-  xlab(label = "Time of Day") + ylab(label = "Violence Level [0-5]") +
-  scale_fill_gradient(name = "sqrt(# Offenses)", low = "#FFFFFF", high = "#012345")
-time.heatmap
-
-# By Day of the Week
-day.heatmap <- ggplot(data = arrests2, mapping = aes(x = arrests2$DayofWeek, y = arrests2$VIOLENCE, 
-                                                         fill= arrests2$sqrt.num)) + geom_tile() +
-  xlab(label = "Day of Week") + ylab(label = "Violence Level [0-5]") +
-  scale_fill_gradient(name = "sqrt(# Offenses)", low = "#FFFFFF", high = "#012345")
-day.heatmap
-
-#By Month of the Year
-month.heatmap <- ggplot(data = arrests2, mapping = aes(x = reorder(arrests2$Month, violenceLevel), y = arrests2$VIOLENCE, 
-                                                         fill= arrests2$sqrt.num)) + geom_tile() +
-  xlab(label = "Month") + ylab(label = "Violence Level [0-5]") +
-  scale_fill_gradient(name = "sqrt(# Offenses)", low = "#FFFFFF", high = "#012345")
-month.heatmap
-
-
-# above code by Emma Graham
+shinyApp(ui, server)
